1d377428a45584b23e56d415e5028953f21c0453
Pixeels <quintero.archie.o@gmail.com>
2021-11-21 02:48:06 +0800

    update

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/', function () {
+    return view('client.index');
+});
+
+Route::get('/employer', function () {
+    return view('client.employer');
+});
+
+Route::get('/applicant', function () {
+    return view('client.applicant');
+});
+
